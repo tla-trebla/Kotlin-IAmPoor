a29f6bd2ea2c0c9632a1e96a7a16fdc45b8f23af
Albert Pangestu <stamina.left@gmail.com>
2024-01-11 19:57:10 +0700

    Added the new launcher icon named `iampoor_launcher` and removed unused comment

--- app/src/main/java/com/example/iampoor/MainActivity.kt
+++ app/src/main/java/com/example/iampoor/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IAmPoorTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Gray) {
